a2133d58c0d26ecde2c0ce1268280ac9a154d7b7
Maxim Dymnov <dymnov.dm@gmail.com>
2024-10-01 04:18:24 -0300

    Add deliverable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,7 +93,6 @@ dependencies {
 
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.ui.test.junit4)
 
     testImplementation(libs.kotest.assertions.core)
